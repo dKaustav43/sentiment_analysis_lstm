2c373f598892bb7e95a0e126443d0da00d7d6a06
dKaustav43 <dkaustav43@gmail.com>
2026-07-20 18:17:27 +0100

    redoing the train test split

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -17,7 +17,11 @@ df = pd.read_csv(base_csv)
 
 #assigning the X,y values and splitting training and testing set - split the training or the test dataset 
 X,y = df['review'].values, df['sentiment'].values
+
+#split for training and testing dataset (80/20)
 x_train, x_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state=42)
+
+#split the training data further into testing, training and validation dataset
 x_train, x_val, y_train, y_val = train_test_split(
     x_train, y_train, test_size=0.2, random_state=42)
 
