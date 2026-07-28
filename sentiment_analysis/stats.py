990a9d9a48cb97bf560d1ab6e59f2ce3a3faa512
dKaustav43 <dkaustav43@gmail.com>
2026-07-28 16:03:46 +0100

    added vocab coverage needed facilitate decision on voacb size.

--- sentiment_analysis/stats.py
+++ sentiment_analysis/stats.py
@@ -1,15 +1,23 @@
 from collections import Counter
 import numpy as np
+import pandas as pd
 from pre_process_and_tokenize import tokenize_text
-from sentiment_analysis import x_train
+from load_imdb_data import load_imdb_data_into_df, train_test_val_set
+from typing import Annotated
+from annotated_types import Interval
 
-def vocab_coverage_stats(tokenized_texts):
+def fraction_word_coverage_distribution(texts:list[str]|pd.Series|np.ndarray) -> tuple[list[float],dict]:
     """
-    Building a function with unique word coverage.
+    cummulative[i] is fraction of all tokens that are accounted for by the top i+1 words. 
     """
-
-    all_tokens = [words for tokens in tokenized_texts for words in tokens]
-  
+    tokenized_texts = tokenize_text(texts)
+    all_tokens = []
+    #first access the row (which is a single review), then loop over the single review appending each word to a list of all_tokens.
+    for tokens in tokenized_texts:
+        for words in tokens:
+            all_tokens.append(words)
+    
+    # create a dictionary mapping for each unique word and how many times it appears
     counter = Counter(all_tokens)
 
     #sort frequencies descending
@@ -17,25 +25,27 @@ def vocab_coverage_stats(tokenized_texts):
     total_tokens = freqs.sum()
 
     #cummulative coverage
-    cumulative = np.cumsum(freqs)/total_tokens
+    cumulative_distribution = np.cumsum(freqs)/total_tokens
 
-    threshold = [0.90,0.95,0.98]
-    coverage_words = {}
+    return cumulative_distribution, counter
 
-    for t in threshold:
-        idx = np.searchsorted(cumulative,t) + 1
-        coverage_words[t] = idx
-
-        print("total unique words in the training set:",len(counter))
-        for t,words_needed in coverage_words.items():
-            print(f"Words needed to cover {int(t*100)}% of tokens:{words_needed}")
-
-        return counter, cumulative
-
+def vocab_coverage_needed(cummulative_distribution:list[float], threshold:Annotated[float, Interval(ge=0,le=1)]):
+    
+    idx = np.searchsorted(cummulative_distribution,threshold) + 1
+    return idx
+    
 def main():
-    train_tokens = tokenize_text(x_train)
+    df_imdb = load_imdb_data_into_df()
+    
+    x_train, x_val, x_test, y_train, y_val, y_test = train_test_val_set(df=df_imdb)
+    
+    cumulative, counter  = fraction_word_coverage_distribution(texts = x_train) 
 
-    counter, cumulative = vocab_coverage_stats(tokenized_texts=train_tokens) # type: ignore
+    print(f"Total number of unique words in the input texts is: {len(counter)}")
+
+    threshold = 0.95
+    idx = vocab_coverage_needed(cummulative_distribution=cumulative, threshold=0.95)
+    print(f"words needed to cover {int(threshold*100)}% of tokens is {idx}")
 
 if __name__ == "__main__":
     main()
