5aeabbe12c9e47563bbce41440c9e178ed53d118
dKaustav43 <dkaustav43@gmail.com>
2026-07-28 11:25:40 +0100

    updated the doc string

--- sentiment_analysis/text_preprocess.py
+++ sentiment_analysis/text_preprocess.py
@@ -1,6 +1,9 @@
 import re
 
 def preprocess_string(text:str):
+    """
+    A basic script to clean and normalize texts.
+    """
     #lowercasing to reduce vocabulary size
     text = text.lower()
     #normalize punctuation - keep !,? for sentiment.
@@ -11,3 +14,12 @@ def preprocess_string(text:str):
     text = re.sub(r"<.*?>", "", text) 
 
     return text
+
+
+def tokenize_text(texts:list[str]) -> list[list[str]]:
+    """
+    Preprocess and tokenize a collection of IMDb reviews.
+    Eac review is cleaned using 'preprocess_string()
+    and then split into a list of white-space separated tokens. 
+    """
+    return [preprocess_string(text).split() for text in texts]
