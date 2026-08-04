d9fe62b85e84f29de8671704bb8047cf0f661a05
dKaustav43 <dkaustav43@gmail.com>
2026-08-04 15:37:53 +0100

    added a print function for trained model parameters

--- sentiment_analysis/main_training.py
+++ sentiment_analysis/main_training.py
@@ -1,12 +1,15 @@
-from sklearn.model_selection import train_test_split
 import numpy as np
 import torch
 import torch.nn as nn
 import time
 from torch.utils.data import DataLoader, TensorDataset
 
-from vocab_and_encoding import encode_train_text_val_dataset
-from load_imdb_data import load_imdb_data_into_df
+from vocab_and_encoding_functions import encode_train_text_val_dataset
+from loading_data_converting_tensor_utils import (load_imdb_data_into_df, 
+                                                  train_test_val_set,
+                                                  encoding_data, 
+                                                 converting_data_to_tensor, 
+                                                 dataloader_batches)
 from lstm_model import LSTMClassifier
 from training_function import training_loop
 
@@ -15,56 +18,26 @@ def main():
 
     device = torch.device("mps" if torch.mps.is_available() else "cpu")
 
-    df = load_imdb_data_into_df()
+    df_imdb = load_imdb_data_into_df()
+    
+    # train-test-val split
+    x_train, x_val, x_test, y_train, y_val, y_test = train_test_val_set(df=df_imdb)
 
-    X = df['review']
-    y = df['sentiment'].values
+    # converting raw data into encodings
+    x_train_encoded, x_val_encoded, x_test_encoded, y_train_encoded, y_val_encoded, y_test_encoded = encoding_data(
+                                                                                x_train, x_val, x_test, y_train, y_val, y_test   
+                                                                                )
 
-    #split for training and testing dataset (80/20)
-    x_train, x_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state=42)
-
-    #split the training data further into training and validation dataset
-    x_train, x_val, y_train, y_val = train_test_split(
-        x_train, y_train, test_size=0.1, random_state=42)
-
-    # info - shape of train data is 36000; shape of test data is 10000; shape of val data is 4000
-
-    ##### encoding data
-
-    x_train_encoded, x_val_encoded, x_test_encoded = encode_train_text_val_dataset(
-                                                                                    x_train_data = x_train,
-                                                                                    x_val_data = x_val,
-                                                                                    x_test_data = x_test
-                                                                                    )
-    y_train = np.array([1 if y == "positive" else 0 for y in y_train])
-    y_val = np.array([1 if y=="positive" else 0 for y in y_val])
-    y_test = np.array([1 if y=="positive" else 0 for y in y_test])
-
-    ### converting to a tensor
-
-    X_train_tensor = torch.tensor(x_train_encoded, dtype=torch.long, device=device)
-    y_train_tensor = torch.tensor(y_train, dtype=torch.float, device=device)
-
-    X_val_tensor = torch.tensor(x_val_encoded, dtype=torch.long, device=device)
-    y_val_tensor = torch.tensor(y_val, dtype=torch.float, device=device)
-
-    X_test_tensor = torch.tensor(x_test_encoded, dtype=torch.long, device=device)
-    y_test_tensor = torch.tensor(y_test, dtype=torch.float, device=device)
-
-    # TensorDataset wraps X and y tensors together so that indexing returns matched (input,label)
-    train_dataset = TensorDataset(X_train_tensor,y_train_tensor)
-    val_dataset = TensorDataset(X_val_tensor,y_val_tensor)
-    test_dataset = TensorDataset(X_test_tensor,y_test_tensor)
-
-    # Creating batches using Dataloader (batch size = 32)
-
-    batch_size = 32
-
-    train_loader = DataLoader(train_dataset,batch_size,shuffle=True)
-
-    val_loader = DataLoader(val_dataset,batch_size, shuffle=True)
-
-    test_loader = DataLoader(test_dataset, batch_size, shuffle = True)
+    # converting encoded data to tensor
+    X_train_tensor, X_val_tensor, X_test_tensor, y_train_tensor, y_val_tensor, y_test_tensor = converting_data_to_tensor(
+      x_train_encoded, x_val_encoded, x_test_encoded, y_train_encoded, y_val_encoded, y_test_encoded,
+      device=device  
+    ) 
+    
+    # dividing the tensors into batches ready for pytorch model
+    train_loader, val_loader, test_loader = dataloader_batches(X_train_tensor, X_val_tensor, X_test_tensor, 
+                                                               y_train_tensor, y_val_tensor, y_test_tensor,
+                                                               batch_size=32)
 
     #Initialising the LSTM classifier model 
 
@@ -90,8 +63,12 @@ def main():
                   seed=42
                   )
     training_end_time = time.perf_counter()
-   
+
     print(f"time taken to run the training loop: {training_end_time - training_start_time}s")
+
+    print("The trained classifier model state dict:")
+    for param_tensor in trained_classifier_model.state_dict():
+        print(param_tensor, "\t", trained_classifier_model.state_dict()[param_tensor].size())
 
 
 if __name__ == main():
