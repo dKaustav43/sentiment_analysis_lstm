e5d2ddf401ca3bdd23fe9560460ac6882f7cc704
dKaustav43 <dkaustav43@gmail.com>
2026-08-04 15:28:47 +0100

    edited the imports and the code to reflect all the abstracted functions to convert raw data into loadable and abtched tensors for pytorch model

--- sentiment_analysis/evaluation.py
+++ sentiment_analysis/evaluation.py
@@ -1,13 +1,14 @@
-from sklearn.model_selection import train_test_split
-import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader
 import time
 
 from lstm_model import LSTMClassifier
-from load_imdb_data import load_imdb_data_into_df
-from vocab_and_encoding import encode_train_text_val_dataset
+from loading_data_converting_tensor_utils import (load_imdb_data_into_df, 
+                                                train_test_val_set, 
+                                                encoding_data, 
+                                                converting_data_to_tensor, 
+                                                dataloader_batches)
 
 # Evaluate one on the test set. Reporting on accuracy, Precision/Recall/F1.
 
@@ -46,50 +47,21 @@ def main():
     device = torch.device("mps" if torch.mps.is_available() else "cpu")
     path_trained_model= "sentiment_analysis/trained_models/lstm_epoch_9.pt"
 
-    df = load_imdb_data_into_df()
+    df_imdb = load_imdb_data_into_df()
 
-    X = df['review']
-    y = df['sentiment'].values
+    x_train, x_val, x_test, y_train, y_val, y_test = train_test_val_set(df=df_imdb)
 
-    #split for training and testing dataset (80/20)
-    x_train, x_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state=42)
+    x_train_encoded, x_val_encoded, x_test_encoded, y_train_encoded, y_val_encoded, y_test_encoded = encoding_data(
+                                                                                x_train, x_val, x_test, y_train, y_val, y_test   
+                                                                                )
 
-    #split the training data further into training and validation dataset
-    x_train, x_val, y_train, y_val = train_test_split(
-        x_train, y_train, test_size=0.1, random_state=42)
-
-    # info - shape of train data is 36000; shape of test data is 10000; shape of val data is 4000
-
-    ##### encoding data
-
-    x_train_encoded, x_val_encoded, x_test_encoded = encode_train_text_val_dataset(
-                                                                                    x_train_data = x_train,
-                                                                                    x_val_data = x_val,
-                                                                                    x_test_data = x_test
-                                                                                    )
-    y_train = np.array([1 if y == "positive" else 0 for y in y_train])
-    y_val = np.array([1 if y=="positive" else 0 for y in y_val])
-    y_test = np.array([1 if y=="positive" else 0 for y in y_test])
-
-    ### converting to a tensor
-
-    X_train_tensor = torch.tensor(x_train_encoded, dtype=torch.long, device=device)
-    y_train_tensor = torch.tensor(y_train, dtype=torch.float, device=device)
-
-    X_test_tensor = torch.tensor(x_test_encoded, dtype=torch.long, device=device)
-    y_test_tensor = torch.tensor(y_test, dtype=torch.float, device=device)
-
-    # TensorDataset wraps X and y tensors together so that indexing returns matched (input,label)
-    train_dataset = TensorDataset(X_train_tensor,y_train_tensor)
-    test_dataset = TensorDataset(X_test_tensor,y_test_tensor)
-
-    # Creating batches using Dataloader (batch size = 32)
-
-    batch_size = 32
-
-    train_loader = DataLoader(train_dataset,batch_size,shuffle=True)
-
-    test_loader = DataLoader(test_dataset, batch_size, shuffle = True)
+    X_train_tensor, X_val_tensor, X_test_tensor, y_train_tensor, y_val_tensor, y_test_tensor = converting_data_to_tensor(
+      x_train_encoded, x_val_encoded, x_test_encoded, y_train_encoded, y_val_encoded, y_test_encoded,
+      device=device  
+    ) 
+    train_loader, val_loader, test_loader = dataloader_batches(X_train_tensor, X_val_tensor, X_test_tensor, 
+                                                               y_train_tensor, y_val_tensor, y_test_tensor,
+                                                               batch_size=32)
 
 
     lstm_model_instance = LSTMClassifier(vocab_size=20002,
