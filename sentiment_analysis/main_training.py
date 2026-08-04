d6535847b11f0b95fda3c2cf89a1d50ca2603d9b
dKaustav43 <dkaustav43@gmail.com>
2026-08-04 14:51:16 +0100

    moved the inference out to a new script

--- sentiment_analysis/main_training.py
+++ sentiment_analysis/main_training.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 import torch
@@ -93,79 +92,6 @@ def main():
     training_end_time = time.perf_counter()
    
     print(f"time taken to run the training loop: {training_end_time - training_start_time}s")
-
-    
-    #Evaluate one on the test set. Reporting on accuracy, Precision/Recall/F1.
-
-    # trained_classifier_model.eval()
-    # test_loss = 0.0
-    # correct = 0
-    # total = 0 
-
-    # with torch.no_grad():
-    #     for X_batch, y_batch in test_loader:
-    #         X_batch = X_batch.to(device)
-    #         y_batch = y_batch.to(device)
-
-    #         logits = trained_classifier_model(X_batch).squeeze(1)
-    #         loss = loss_fn(logits,y_batch)
-    #         test_loss += loss.item()
-
-    #         preds = (torch.sigmoid(logits)>0.50).long()
-    #         correct += (preds == y_batch.long()).sum().item()
-    #         total += y_batch.size(0)
-
-    #     print(
-    #         f"Test loss: {test_loss/len(train_loader):.4f} | "
-    #         f"Test Acc: {correct/total:.4f}"
-    #     )   
-
-
-
-    # vocab = build_vocab(train_tokens,max_vocab_size=20000)
-
-
-    # # Saving the model
-    # # classifier_model = LSTMClassifier(vocab_size=20002,  # +2 for PAD and UNK
-    # #                                   embed_dim=100,
-    # #                                   hidden_dim=128).to(device)
-
-
-
-    # #Inference function/layer.
-
-
-    # def predict_sentiment(
-    #         text:str,
-    #         vocab=vocab,
-    #         max_length=300,
-    #         device=device
-    # ):
-    #     classifier_model.eval()
-    #     #tokenize
-    #     tokens = text.lower().split()
-
-    #     #encode
-    #     encoded = encode_tokens(tokens, vocab)
-    #     encoded = truncation(encoded, max_length)
-    #     encoded = pad_sequence(encoded, max_length)
-
-
-    #     x = torch.tensor(encoded).unsqueeze(0).to(device)
-
-    #     with torch.no_grad():
-    #         logit = classifier_model(x).squeeze()
-    #         prob = torch.sigmoid(logit).item()
-
-    #     label = "positive" if prob >= 0.5 else "negetive"
-
-    #     return label, prob
-
-
-    # label,prob = predict_sentiment(
-    #     "The movie was was well done!"
-    # )
-    # print(label,prob)
 
 
 if __name__ == main():
