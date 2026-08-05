eb7f48d4d710390212a9c8652ae1344a01ea1f06
dKaustav43 <dkaustav43@gmail.com>
2026-08-05 15:58:49 +0100

    restricted to only loading and train_test_val_split. removed other functions

--- sentiment_analysis/loading_imdb_train_test_val_split.py
+++ sentiment_analysis/loading_imdb_train_test_val_split.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import numpy as np
-import torch
-from torch.utils.data import DataLoader, TensorDataset
 import time
 from pathlib import Path
 from sklearn.model_selection import train_test_split
@@ -33,48 +30,7 @@ def train_test_val_set(df:pd.DataFrame):
     
     return x_train, x_val, x_test, y_train, y_val, y_test
 
-def encoding_data(x_train,x_val,x_test,y_train,y_val,y_test):
 
-    from vocab_and_encoding_functions import encode_train_text_val_dataset
-    
-    x_train_encoded, x_val_encoded, x_test_encoded = encode_train_text_val_dataset(
-                                                                                    x_train_data = x_train,
-                                                                                    x_val_data = x_val,
-                                                                                    x_test_data = x_test
-                                                                                    )
-    y_train_encoded = np.array([1 if y == "positive" else 0 for y in y_train])
-    y_val_encoded = np.array([1 if y=="positive" else 0 for y in y_val])
-    y_test_encoded = np.array([1 if y=="positive" else 0 for y in y_test])
-
-    return x_train_encoded, x_val_encoded, x_test_encoded, y_train_encoded, y_val_encoded, y_test_encoded
-
-def converting_data_to_tensor(x_train_encoded, x_val_encoded, x_test_encoded, y_train_encoded, y_val_encoded, y_test_encoded, device):
-    
-    X_train_tensor = torch.tensor(x_train_encoded, dtype=torch.long, device=device)
-    y_train_tensor = torch.tensor(y_train_encoded, dtype=torch.float, device=device)
-
-    X_val_tensor = torch.tensor(x_val_encoded, dtype=torch.long, device=device)
-    y_val_tensor = torch.tensor(y_val_encoded, dtype=torch.float, device=device)
-
-    X_test_tensor = torch.tensor(x_test_encoded, dtype=torch.long, device=device)
-    y_test_tensor = torch.tensor(y_test_encoded, dtype=torch.float, device=device)
-
-    return X_train_tensor, X_val_tensor, X_test_tensor, y_train_tensor, y_val_tensor, y_test_tensor
-
-def dataloader_batches(x_train_tensor,x_val_tensor,x_test_tensor, y_train_tensor, y_val_tensor, y_test_tensor,
-                        batch_size:int = 32):
-
-    train_dataset = TensorDataset(x_train_tensor,y_train_tensor)
-    val_dataset = TensorDataset(x_val_tensor,y_val_tensor)
-    test_dataset = TensorDataset(x_test_tensor,y_test_tensor)
-
-    train_loader = DataLoader(train_dataset,batch_size,shuffle=True)
-
-    val_loader = DataLoader(val_dataset,batch_size, shuffle=True)
-
-    test_loader = DataLoader(test_dataset, batch_size, shuffle = True)
-
-    return train_loader, val_loader, test_loader
 
 def main():
     start_time = time.perf_counter()
